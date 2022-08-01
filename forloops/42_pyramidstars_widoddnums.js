d52902e060e237f126bf1ebb6675a3428e9db162
Your Name <you@example.com>
2022-08-01 21:55:59 +0530

    42_pyramid_wid_oddnums

---
+++ forloops/42_pyramidstars_widoddnums.js
@@ -0,0 +1,14 @@
+let str =" ";
+    let l = 1;
+    let num=5;
+        for(let i=0;i<=num;i++){
+    
+            for(let j=num-i;j>=0;j--)
+                str +=" ";
+    
+            for(let k=1;k<=2*i-1;k++){
+                str += "* ";
+            }
+            console.log(str);
+            str=" ";
+        }
